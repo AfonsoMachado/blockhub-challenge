9a4b8aced5ed3e76270062e02e9b63b459623b31
Afonso Machado <afonso777@hotmail.com>
2021-08-07 14:08:35 -0300

    base da tela de login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     Tela de login
+    <div id="login-box">
+      <form>
+        Usuário
+        <input type="text" name="e-mail" id="">
+        Senha
+        <input type="password" name="password" id="">
+
+        <button type="submit">Login</button>
+        <button>Cadastre-se</button>
+      </form>
+    </div>
   </div>
 </template>
 
