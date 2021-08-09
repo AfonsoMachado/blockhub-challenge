159a71b0adba040759de32c1feda1444709ab878
Afonso Machado <afonso777@hotmail.com>
2021-08-09 15:47:12 -0300

    arquivo de requisição axios

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    Cadastro de usuário
+  <div class="container">
+    Cadastro de Usuário
+    <div id="register-box">
+      <form>
+        <div class="form">
+          <legend>Nome</legend>
+          <input type="text" name="name" id="">
+          <legend>E-mail</legend>
+          <input type="text" name="e-mail" id="">
+          <legend>Senha</legend>
+          <input type="password" name="password" id="">
+        </div>
+        
+        
+
+        <div id="botoes">
+          <button type="submit">Cadastrar-se</button>
+        </div>
+
+        
+      </form>
+    </div>
   </div>
 </template>
 
@@ -11,5 +31,15 @@ export default {
 </script>
 
 <style>
+
+#register-box {
+  width: 30%;
+}
+
+#botoes {
+  display: flex;
+  justify-content: center;
+  margin-top: 5%;
+}
 
 </style>
