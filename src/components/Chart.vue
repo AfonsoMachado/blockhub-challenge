506c3c93e62f2e77a8b8906bca6be22b98e6233c
Afonso Machado <afonso777@hotmail.com>
2021-08-12 17:35:39 -0300

    exibindo grafico em pizza corretamente

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -7,6 +7,12 @@ export default {
     chartdata: [],
     labelsdata: [],
   },
+  data() {
+    return {
+      uselabelsdata: [],
+      usechartdata: [],
+    };
+  },
   // mounted() {
   //   console.log(this.chartdata);
   //   this.chartdata = [300, 50, 100];
@@ -14,21 +20,55 @@ export default {
   // },
 
   mounted() {
+    this.uselabelsdata = this.labelsdata;
+    this.usechartdata = this.chartdata;
+    //  gerando cores
+    let bgs = [];
+    for (let i = 0; i < this.chartdata.length; i++) {
+      bgs[i] = this.getRandomColor();
+    }
+
+    // ajustando array somente com projetos disponiveis
+    // eslint-disable-next-line no-unused-vars
+
+    // this.usechartdata = this.usechartdata.filter(this.notZero);
+
+    // for (let i = 0; i < this.chartdata.length; i++) {
+    //   if (this.chartdata[i] == 0) {
+    //     console.log("passoussdaasdasdsadfsdafgsadfads");
+    //     this.uselabelsdata.splice(i, 1);
+    //   }
+    // }
+
+    console.log("NOVO ARRAY", this.usechartdata);
+    console.log("NOVO ARRAY 2", this.uselabelsdata);
+
     this.renderChart({
-      labels: this.labelsdata.map((project) => project.name),
+      labels: this.uselabelsdata.map((project) => project.name),
       datasets: [
         {
           label: "Horas trabalhadas por projeto",
-          data: this.chartdata,
-          backgroundColor: [
-            "rgb(255, 99, 132)",
-            "rgb(54, 162, 235)",
-            "rgb(255, 205, 86)",
-          ],
+          data: this.usechartdata,
+          backgroundColor: bgs,
           hoverOffset: 4,
         },
       ],
     });
+  },
+
+  methods: {
+    notZero(n) {
+      return n != 0;
+    },
+    // gera cores aleatorias
+    getRandomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
   },
 };
 </script>
